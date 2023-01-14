236d8697155708163a1d58c7f7f3c1ccb2c992af
Damian9604 <115879834+Damian9604@users.noreply.github.com>
2023-01-14 13:28:46 +0100

    1 komponent

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'React';
+class EmployeesListComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>EmployeeList</p>
+            </div>
+        )
+    }
+}
+export default EmployeesListComponent;
